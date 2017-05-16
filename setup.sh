b3973d5655944c545376e45fdc3ca8c4d7b1be6e
Thomas Hart <myrridin@gmail.com>
2017-05-15 20:39:21 -0400

    Update setup.sh and remove included plugins

--- setup.sh
+++ setup.sh
@@ -2,10 +2,22 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Copy bash_profile contents over
-cat $DIR/.bash_profile > ~/.bash_profile
+cat $DIR/.bash_profile >> ~/.bash_profile
 
 # Set up symbolic links
 ln -s $DIR/.vim ~/.vim
 ln -s $DIR/.vimrc ~/.vimrc
 
-echo "Don't forget to install ncurses-term"
+# Install VIM Plugins
+mkdir ~/.vim/bundle
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/valloric/youcompleteme
+git clone https://github.com/tpope/vim-rails
+git clone https://github.com/vim-ruby/vim-ruby
+git clone https://github.com/thoughtbot/vim-rspec
+git clone https://github.com/blueshirts/darcula
+git clone https://github.com/tpope/vim-sensible
